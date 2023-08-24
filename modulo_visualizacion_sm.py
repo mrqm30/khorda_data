528bd5094d5bed53414c0d01a7dac62b95f3cda1
Waters88 <107167376+Waters88@users.noreply.github.com>
2023-08-23 22:16:00 -0600

    Plantillas proyecto Khorda

---
+++ modulo_visualizacion_sm.py
@@ -0,0 +1,11 @@
+class Visualization:
+    def _init__(self, *args):
+        self.historico = self.arg[0]
+        self.polaridad = self.arg[1]
+        self.graf_pendiente = self.arg[2]
+    def serie_tiempo(self):
+        return fig_serie
+    def pie(self):
+        return fig_pie
+    def pendiente(self):
+        return fig_pendiente
